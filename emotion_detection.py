ab194cde1d9a004fc940da8cb9678c36b13a1923
zanadebo <128149612+zanadebo@users.noreply.github.com>
2026-02-10 16:35:06 +0100

    Add files via upload

---
+++ emotion_detection.py
@@ -0,0 +1,43 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    if not text_to_analyse:
+        return {
+            'anger': 0,
+            'disgust': 0,
+            'fear': 0,
+            'joy': 0,
+            'sadness': 0,
+            'dominant_emotion': None
+        }
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
+        "Content-Type": "application/json"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=input_json)
+    response.raise_for_status()
+    data = response.json()
+
+    emotions = data["document"]["emotion"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
+
